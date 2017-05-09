4fc85f53e08f18299be1558fd05ceff009db5072
rayGreycloud <rayGreycloud@gmail.com>
2017-05-09 11:16:50 -0700

    Add setupController to app

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
+var setupController = require('./controllers/setupController');
+
 var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/publics'));
@@ -9,5 +11,6 @@ app.use('/assets', express.static(__dirname + '/publics'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
+setupController(app);
 
 app.listen(port);
